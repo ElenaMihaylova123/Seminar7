da7d263c7ec3a7c8e727f6789f466e8efad7afbe
ElenaMihaylova <elena.16.04@mail.ru>
2023-06-01 20:10:17 +0700

    All done

---
+++ task50/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 50. Вывести элемент двумерного массива по его позиции, которую задаёт пользователь.
+
+
+Console.WriteLine("Введите размеры массива через Enter");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите координаты через Enter");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m || b>n)
+    Console.WriteLine("такого элемента нет");
+ else
+    {
+        object c = array.GetValue(a-1,b-1);
+        Console.WriteLine(c);
+    }
